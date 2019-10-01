139b183f80f89148934299afd766ce03d85f85d1
whliang2 <whliang2@illinois.edu>
2019-10-01 10:45:23 -0500

    1001 Update

---
+++ 1001_Update/imageprocess.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def main():
+    imgpath = "/Users/Henry/Desktop/view.jpg" #put your image path here
+    img1 = cv2.imread(imgpath, 0)
+
+    print(img1)
+    print(img1.dtype)
+    print(img1.shape)
+    print(img1.ndim)
+    print(img1.size)
+
+
+    cv2.imshow('a',img1)
+    cv2.waitKey(0)
+    cv2.destroyWindow('a')
+
+if __name__ == "__main__":
+    main()
