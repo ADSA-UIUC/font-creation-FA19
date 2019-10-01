31b4cfcee0f7e13509ab55a60c71ddc7682e5b5d
whliang2 <whliang2@illinois.edu>
2019-10-01 11:07:37 -0500

    update

--- 1001_Update/imageprocess.py
+++ 1001_Update/imageprocess.py
@@ -1,20 +1,25 @@
 import cv2
 
-
+vecror_i=[]
 def main():
     imgpath = "/Users/Henry/Desktop/view.jpg" #put your image path here
     img1 = cv2.imread(imgpath, 0)
-
-    print(img1)
-    print(img1.dtype)
-    print(img1.shape)
-    print(img1.ndim)
-    print(img1.size)
+    vecror_i.append(img1)
 
 
-    cv2.imshow('a',img1)
-    cv2.waitKey(0)
-    cv2.destroyWindow('a')
 
+#     print(img1)
+#     print(img1.dtype)
+#     print(img1.shape)
+#     print(img1.ndim)
+#     print(img1.size)
+#
+#
+#     cv2.imshow('a',img1)
+#     cv2.waitKey(0)
+#     cv2.destroyWindow('a')
+#
 if __name__ == "__main__":
     main()
+
+print(vecror_i)
